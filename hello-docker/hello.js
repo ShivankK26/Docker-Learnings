95211c4eb50752010db440a0656c43971fad6c01
Shivank Kapur <shivankkapur@Shivanks-MacBook-Pro.local>
2024-06-25 22:10:13 +0530

    first commit

---
+++ hello-docker/hello.js
@@ -0,0 +1,11 @@
+console.log("hey docker!");
+
+
+// Command for buliding Dockerfile
+// docker build -t hello-docker .
+
+// Command to view images created - docker images
+
+// Command for running Dockerfile - docker run hello-
+
+// Command to run a file inside an OS in docker - docker run -it hello-docker sh
