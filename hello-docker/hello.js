811c9c3820dd2a3722248d8e3f82fda1be60bafb
Shivank Kapur <shivankkapur@Shivanks-MacBook-Pro.local>
2024-06-27 23:44:38 +0530

    added docker for react

--- hello-docker/hello.js
+++ hello-docker/hello.js
@@ -1,11 +1,11 @@
 console.log("hey docker!");
 
 
-// Command for buliding Dockerfile
+// Command for building Dockerfile
 // docker build -t hello-docker .
 
 // Command to view images created - docker images
 
-// Command for running Dockerfile - docker run hello-
+// Command for running Dockerfile - docker run hello-docker
 
 // Command to run a file inside an OS in docker - docker run -it hello-docker sh
