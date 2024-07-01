a4c46a1a8be63fdd9068f848cd33bedee9b1434a
Shivank Kapur <shivankkapur2004@gmail.com>
2024-07-01 13:18:04 +0530

    learnt docker commpose watch & yaml files

---
+++ mern-docker/frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav className="navbar">
+        <Link to="/" className="navbar_title">
+          <h1>Anime</h1>
+        </Link>
+        <div className="navbar_links">
+          <Link to="/" className="navbar_link">
+            Home
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Share
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Explore
+          </Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
